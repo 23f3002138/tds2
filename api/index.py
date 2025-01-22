61b45dab2747eac88ed1f6674c5bd4cfbc02af44
GirishIITM <23f2005217@ds.study.iitm.ac.in>
2025-01-22 12:24:31 +0530

    Enhance API functionality and add marks data handling

--- api/index.py
+++ api/index.py
@@ -1,18 +1,42 @@
-from flask import Flask
+from flask import Flask, request, jsonify
+import json
 
 app = Flask(__name__)
 
-@app.route('/')
+
+@app.route("/")
 def home():
-    return 'it works!'
+    return "it works!"
 
-@app.route('/api', methods=['GET'])
+
+@app.route("/api", methods=["GET"])
 def get_marks():
-    with
-    names = request.args.getlist('name')
-    response = {name: marks_data.get(name, "Name not found") for name in names}
-    return jsonify(response)
+    try:
+        with open("./q-vercel-python.json") as f:
+            marks_data_arr = json.load(f)
+        f.close()
+        names = request.args.getlist("name")
+        if names is not None:
+            marks = []
+            for name in names:
+                for data in marks_data_arr:
+                    print(data)
+                    if data["name"] == name:
+                        marks.append(data.get("marks"))
+            print(marks)
+            return jsonify({"marks": marks})
+        else:
+            return jsonify({"marks": marks_data_arr})
 
-@app.route('/about')
+    except Exception as e:
+        return jsonify({"error": str(e)})
+
+
+@app.route("/about")
 def about():
-    return 'About'
+    return "About"
+
+
+# run the app
+# if __name__ == "__main__":
+#     app.run()
