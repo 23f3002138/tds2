3ac9373a3d47f747aab0ca06374a0d48c20223f8
Girish Vishveshvara Bhat <159102618+GirishIITM@users.noreply.github.com>
2025-01-22 12:00:30 +0530

    Update index.py

--- api/index.py
+++ api/index.py
@@ -4,7 +4,14 @@ app = Flask(__name__)
 
 @app.route('/')
 def home():
-    return 'Hello, World!'
+    return 'it works!'
+
+@app.route('/api', methods=['GET'])
+def get_marks():
+    with
+    names = request.args.getlist('name')
+    response = {name: marks_data.get(name, "Name not found") for name in names}
+    return jsonify(response)
 
 @app.route('/about')
 def about():
